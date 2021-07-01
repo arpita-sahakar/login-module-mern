1a922d2b0efa487d3c98075c8eab50f8653beb49
Arpita Saha <arpita.sahaa@gmail.com>
2021-07-01 17:55:09 -0400

    created skeleton for client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/signUpComponent/SignUp';
+import Login from './components/loginComponent/Login';
+import Display from './components/displayComponent/Display';
 
 function App() {
   return (
     <div className="App">
-   <h1>Hello</h1>
+      <h1>Hello</h1>
+      <SignUp />
+      <Login />
+      <Display />
     </div>
   );
 }
---
+++ client/src/components/displayComponent/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Display() {
+    return (
+        <div>
+            <h2>Welcome Arpita</h2>
+        </div>
+    )
+}
+
+export default Display
